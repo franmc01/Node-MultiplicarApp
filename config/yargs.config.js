ccfeefeb77eec22687cc08d13a0a916c13d284fa
francisco-1998 <francmarinc@gmail.com>
2021-02-23 13:39:02 -0500

    Retoques a la info de ayuda

--- config/yargs.config.js
+++ config/yargs.config.js
@@ -2,19 +2,22 @@ const argv = require('yargs')
     .option('b', {
         alias: 'base',
         type: 'number',
-        demandOption: true
+        demandOption: true,
+        desc: 'Es el numero de la tabla de multiplicar a generar'
 
     })
     .option('l', {
         alias: 'listar',
         type: 'boolean',
-        default: true
+        default: true,
+        desc: 'Permite ver o ocultar el resultado de la tabla generada en la consola'
 
     })
     .option('t', {
-        alias: 'crear_txt',
+        alias: 'txt',
         type: 'boolean',
-        default: false
+        default: false,
+        desc: 'Permita crear o no un archivo .txt con los datos de la tabla generada'
 
     })
     .check((argv, options) => {
