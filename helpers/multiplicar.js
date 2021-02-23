74c169f55d86849aed570afb5082471a147ef37c
francisco-1998 <francmarinc@gmail.com>
2021-02-23 13:57:15 -0500

    Coloreando xD

--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -1,27 +1,34 @@
 const fs = require('fs');
+const colors = require('colors');
 
 const multiplicar = async (base, listar, txt) => {
 
     try {
         //Elabora la tabla de multiplicacion
         let salida = '';
+        let mensaje = '';
 
         for (let i = 1; i <= 10; i++) {
-            salida += `${base} x ${i} = ${base * i}\n`;
+            salida += `${base} ${'x'.yellow} ${i} ${colors.magenta('=')} ${base * i}\n`;
         }
 
         if(listar){
             //Imprime en consola la tabla
+            console.clear();
             console.log('----------------------');
             console.log('     Tabla del: ', base);
             console.log('----------------------');
             console.log(salida);
+
+            mensaje = 'Tabla generada con exito';
         }
 
         if(txt){
             fs.writeFileSync(`tabla-${base}.txt`, salida);
-            return `Archivo con la tabla del ${base} creado correctamente`;
+            mensaje = `Archivo con la tabla del ${base} creado correctamente`;
         }
+
+        return mensaje;
     } catch (error) {
         throw error
     }
